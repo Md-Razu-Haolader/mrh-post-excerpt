cc0c936b188bd86e9bc72cd4864be23c4292a488
Md. Razu Haolader <razu.cse129@gmail.com>
2023-07-05 21:04:40 +0600

    Update plugin activate

--- includes/PostExcerpt.php
+++ includes/PostExcerpt.php
@@ -52,7 +52,7 @@ final class PostExcerpt {
      * Initialize hooks.
      */
     private function init_hooks(): void {
-        register_activation_hook( __FILE__, [$this, 'activate'] );
+        register_activation_hook( MRHPE_FILE, [$this, 'activate'] );
         add_action( 'plugins_loaded', [$this, 'init_classes'] );
     }
 
